2bf0c518429ae72633dc4d8466ced90805421380
Rohit Khatri <developer.rohitkhatri@gmail.com>
2018-05-31 09:11:25 +0530

    v1.0.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except IOError:
 
 setup(
     name='youtube-python',
-    version='1.0.4',
+    version='1.0.5',
     description='Python Youtube Data API v3',
     long_description=long_description,
     url='https://github.com/rohitkhatri/youtube-python',
