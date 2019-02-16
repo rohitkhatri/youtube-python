4e9558b286be939c0e7db1d5c23856f7a2333f74
rohitkhatri <rohit@thoughtchimp.com>
2019-02-17 00:19:56 +0530

    Pypi package version updated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except IOError:
 
 setup(
     name='youtube-python',
-    version='1.0.12',
+    version='1.0.13',
     description='Python Youtube Data API v3',
     long_description=long_description,
     url='https://github.com/rohitkhatri/youtube-python',
